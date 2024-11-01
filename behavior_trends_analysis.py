a7ff8007d04a59ce3b7bad3235ba25a0c000b4c8
syj24psx <syj24psx@bangor.ac.uk>
2024-11-01 10:07:03 +0000

    Update behavior_trends_analysis.py

--- behavior_trends_analysis.py
+++ behavior_trends_analysis.py
@@ -1,1 +1,9 @@
 import pandas as pd
+def import_data(filename: str) -> pd.DataFrame:
+    if filename.endswith('Customer_Behavior.xlsx'):
+        return pd.read_excel(Customer_Behavior)
+
+def filter_data(df: pd.DataFrame) -> pd.DataFrame:
+    filtered_df = df.dropna(subset=['CustomerID'])
+    filtered_df = filtered_df[(filtered_df['Quantity'] >= 0) & (filtered_df['UnitPrice'] >= 0)]
+    return filtered_df
